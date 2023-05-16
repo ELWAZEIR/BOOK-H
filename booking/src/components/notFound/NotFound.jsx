993df0ada14d28ad16f5c66192c74764c16693cf
Hossam shaaban <99195138+ELWAZEIR@users.noreply.github.com>
2023-05-17 02:35:31 +0300

    Update NotFound.jsx

--- booking/src/components/notFound/NotFound.jsx
+++ booking/src/components/notFound/NotFound.jsx
@@ -6,7 +6,7 @@ function NotFound() {
       <div class="error">404</div>
       <br />
       <br />
-      <span class="info">This page not found</span>
+      <span class="info"> <h1> This app is not working because you are using mongo data bias</h1> </span>
       <img
         src="https://static-00.iconduck.com/assets.00/robot-404-error-errors-illustration-512x427-j192mt0z.png"
         className="static"
